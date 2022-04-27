c5b78338a59123dd3c3e6bb527f77f28fe2eb2ef
unknown <xdipcar@ad.fi.muni.cz>
2022-04-27 12:44:50 +0200

    fix some issues

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,9 +1,12 @@
-# contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+"""
+Contains bunch of buggy examples
+Taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+"""
+
+import subprocess
 import cPickle
 import subprocess
 import base64
-import subprocess
 import flask
 
 # Input injection
@@ -13,21 +16,28 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
+def asserts(request, user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
 
 # Pickles
-class RunBinSh(object):
+class RunBinSh():
+    '''Doc string for RunBinSh class'''
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
 
+    def random_method(self):
+        '''Doc string for random_method'''
+        return "random text"
+
 def import_urlib_version(version):
+    '''Doc string for import_urlib_version'''
     exec("import urllib%s as urllib" % version)
 
 @app.route('/')
 def index():
+    '''Doc string for index'''
     module = flask.request.args.get("module")
     import_urlib_version(module)
 
